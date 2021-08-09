7509d7f8653c793679ad4f474f87d9f99bd32ce5
Ziyad <62690285+ZiyadAhmed910@users.noreply.github.com>
2021-08-09 13:45:09 +0530

    Update index.php

--- IOT-CLOUD/index.php
+++ IOT-CLOUD/index.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<meta name="Vikkey" content="Vivek Gupta & IoTMonk">
+	<meta name="ziyad" content="ziyad">
 	<meta http-equiv="Access-Control-Allow-Origin" content="*">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 	<link rel="stylesheet" href="styless.css">
